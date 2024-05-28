a8cd1c91c07f8d219e0cbf9c37fb2643f7e8b5cb
ByteSpectre <magomedovmagush@gmail.com>
2024-05-29 00:41:44 +0300

    Step 7 + lint fix

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,19 +1,23 @@
+#!/usr/bin/env python3
 import prompt
 import random
 from brain_games.cli import welcome_user
 
 
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
 def main():
     name = welcome_user()
-    print('What is the result of the expression?')
     count = 0
     while count < 3:
-        random_int_1 = random.randint(1, 10)
-        random_int_2 = random.randint(1, 10)
-        action = random.choice(['*', '+', '-'])
-        operation = ' '.join((str(random_int_1), action, str(random_int_2)))
-        result = int(eval(operation))
-        print(f'Question: {operation}')
+        random_int_1 = random.randint(1, 100)
+        random_int_2 = random.randint(1, 100)
+        print('Question:', random_int_1, random_int_2)
+        result = gcd(random_int_1, random_int_2)
         answer = prompt.integer('Your answer: ')
         if answer == result:
             print('Correct!')
@@ -22,7 +26,6 @@ def main():
             print(f"'{answer}' is wrong answer ;(."
                   + f" Correct answer was '{result}'. \n"
                   + f"Let's try again, {name}!")
-    print(f'Congratulations, {name}!')
 
 
 if __name__ == '__main__':
